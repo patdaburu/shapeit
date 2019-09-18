36224738eebde65d60c26ce81c87fce3f7c4a2fa
Pat Daburu <patdaburu@gmail.com>
2019-09-18 07:59:04 -0500

    Updated the version.

--- shapeit/version.py
+++ shapeit/version.py
@@ -8,5 +8,5 @@
 This module contains project version information.
 """
 
-__version__ = '0.0.4'  #: the working version
-__release__ = '0.0.4'  #: the release version
+__version__ = '0.0.5'  #: the working version
+__release__ = '0.0.5'  #: the release version
